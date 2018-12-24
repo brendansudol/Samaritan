74236d79d73c74614fe404083b1e760427746289
brendan sudol <brendansudol@gmail.com>
2018-12-24 13:08:32 -0500

    init

---
+++ TODO.md
@@ -0,0 +1,9 @@
+-   Landing screen
+-   App walkthrough
+    -   3-4 swipeable sections
+-   Day view
+    -   today's date
+    -   3 cards with charity info / action buttons
+-   Charity detail view
+
+-   Authentication screens
---
+++ screens/HomeScreen.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={require('../assets/images/icon.png')}
+              style={styles.welcomeImage}
+            />
+          </View>
+          <View style={styles.getStartedContainer}>
+            <Text style={styles.getStartedText}>
+              Lorem ipsum dolor sit amet...
+            </Text>
+          </View>
+        </ScrollView>
+
+        <View style={styles.tabBarInfoContainer}>
+          <Text style={styles.tabBarInfoText}>Lorem ipsum dolor sit amet.</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+})
